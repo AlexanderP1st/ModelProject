935b092ab2f405a30e3c7ea7a31bcbaba7297e9e
AlexanderP1st <alexander.p.backup@gmail.com>
2024-10-25 09:57:48 +0100

    I created some providers , fixed some errors and worked on the Upload page feature.

---
+++ ModelProject/Context/ModelProvider.cs
@@ -0,0 +1,16 @@
+﻿using ModelProject.Model;
+using System.Collections.Generic; 
+public class ModelProvider
+{
+    private readonly List<DigitalModel> models = new();
+
+    public void AddModel(DigitalModel model)
+    {
+        models.Add(model); 
+    }
+    
+    public List<DigitalModel> GetAllModels()
+    {
+        return models; 
+    }
+}
---
+++ ModelProject/Context/UserProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ModelProject.Model;
+
+namespace ModelProject.Context
+{
+    public class UserProvider
+    {
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public UserProvider(DatabaseContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager; 
+        }
+        public User? GetUserByUsername(string? username)
+        {
+            //returns the user with the specified username
+            return _context.Users.FirstOrDefault(user => user.UserName == username);
+        }
+    }
+}
