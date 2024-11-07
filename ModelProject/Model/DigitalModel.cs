8b375e9f4405b984be3b5c1013f96bcbaca13304
AlexanderP1st <alexander.p.backup@gmail.com>
2024-11-07 09:33:17 +0000

    few changes

--- ModelProject/Model/DigitalModel.cs
+++ ModelProject/Model/DigitalModel.cs
@@ -1,19 +1,31 @@
-﻿namespace ModelProject.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelProject.Model
 {
     public class DigitalModel
     {
         public int Id { get; set; }
 
         public User User { get; set; }
+
+        [Required]
+       
         public string Title { get; set; }
 
+        [Required]
         public string[] ModelFiles { get; set; }
 
-        public string[] Images { get; set; } 
+        [Required]
+        public string[] Images { get; set; }
+
+        [Required]
         public string Category { get; set; }
 
+        [Required]
+        
         public string Description { get; set; }
 
+        [Required]
         public string Specifications { get; set; }
 
         public int Downloads { get; set; }
