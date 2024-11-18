922cf7418d06498b2618012a80c9e72871139e89
AlexanderP1st <alexander.p.backup@gmail.com>
2024-11-18 15:40:34 +0000

    got a few errors to fix

--- ModelProject/Context/ModelProvider.cs
+++ ModelProject/Context/ModelProvider.cs
@@ -1,15 +1,16 @@
 ﻿using ModelProject.Model;
-using SQLitePCL;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using ModelProject.Context;
+
+
+
 public class ModelProvider
 {
     private readonly DatabaseContext _context;
 
     public ModelProvider(DatabaseContext context)
     {
-        _context = context;
+      _context = context;
     }
 
     public async Task AddModelAsync(DigitalModel digitalModel)
@@ -17,19 +18,20 @@ public class ModelProvider
         _context.DigitalModels.Add(digitalModel);
         await _context.SaveChangesAsync();
     }
-    
+
     public async Task UpdateModelAsync(DigitalModel digitalModel)
     {
         _context.DigitalModels.Update(digitalModel);
         await _context.SaveChangesAsync();
 
     }
-    
-   public async Task<List<DigitalModel>> GetAllModelsAsync()
+
+    public async Task<List<DigitalModel>> GetAllModelsAsync()
     {
-        return await _context.DigitalModels.ToListAsync();
+        return await _context.DigitalModels.OrderBy(digitalModel => digitalModel.Title).ToListAsync();
     }
 
 
-   
+
 }
+
