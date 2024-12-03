a18affd67a6903e08caa05cebcfe4980752025e6
AlexanderP1st <alexander.p.backup@gmail.com>
2024-12-03 15:34:02 +0000

    Working on image file uploader class

---
+++ ModelProject/Context/FileUploader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+public class FileUploader
+{
+    private readonly IWebHostEnvironment _environment;
+    private const long MAX_FILE_SIZE = 1024 * 1024 * 1024 * 5;
+    private readonly string[] _allowedExtentions = [".jpg", ".jpeg", ".png"]; 
+
+    public FileUploader(IWebHostEnvironment environment)
+    {
+        _environment = environment; 
+    }
+
+    public async Task<string?> UploadFileAsync(IBrowserFile file)
+    {
+        var imageFolder = Path.Combine(_environment.WebRootPath, "img", "digitalmodels");
+        if (!Directory.Exists(imagesFolder)) Directory.CreateDirectory(imagesFolder);
+
+
+        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.Name); 
+    }
+}//KEEP WORKING ON THIS 
