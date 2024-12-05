097a5f61f9d6514776f7d54fb43701048acea07c
Alexander Price <alexander.p.backup@gmail.com>
2024-12-05 00:36:35 +0000

    Latest changes

--- ModelProject/Context/FileUploader.cs
+++ ModelProject/Context/FileUploader.cs
@@ -6,7 +6,9 @@ namespace ModelProject.Context
     public class FileUploader
     {
         private readonly IWebHostEnvironment _environment;
+        //Maximum allowed file size in bytes (5MB) 
         private const long MAX_FILE_SIZE = 1024 * 1024 * 5;
+        //types of file allowed to upload 
         private readonly string[] _allowedExtensions = [".jpg", ".jpeg", ".png"];
 
         public FileUploader(IWebHostEnvironment environment)
@@ -16,14 +18,16 @@ namespace ModelProject.Context
 
         public async Task<string?> UploadFileAsync(IBrowserFile file)
         {
+            //combines the web root path with subfolders 
             var imagesFolder = Path.Combine(_environment.WebRootPath, "img", "digitalmodels");
             if (!Directory.Exists(imagesFolder)) Directory.CreateDirectory(imagesFolder);
 
-
+            //generates a unique file name 
             var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.Name);
             var filePath = Path.Combine(imagesFolder, fileName);
             var fileExtension = Path.GetExtension(file.Name);
 
+            //rejects models over the MAX_FILE_SIZE (5MB) 
             if (file.Size > MAX_FILE_SIZE) return null;
             if (!_allowedExtensions.Contains(fileExtension)) return null;
 
